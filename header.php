27f26161051509be72baacbc86fd94b813d2f6bb
Ali ABBAS <ali.ibra.abbas@gmail.com>
2022-12-30 18:01:35 +0100

    :white_check_mark: ADD header

--- header.php
+++ header.php
@@ -12,17 +12,19 @@
     <link rel="stylesheet" href="style.css" >
 </head>
 <body >
-
-
     <nav >
             <ul>
                 <a  href="index.php"><li>Home</li></a>
+
                 <?php if(!isset($_SESSION['usersId'])) : ?>
-                    <a  href="signup.php"><li>Sign Up</li></a>
-                    <a  href="login.php"><li>Login</li></a>
-                    <?php else: ?>
-                <a href="./controllers/Users.php?q=logout"><li>Logout</li></a>
-            <?php endif; ?>
+
+                        <a  href="signup.php"><li>Sign Up</li></a>
+                        <a  href="login.php"><li>Login</li></a>
+
+                        <?php else: ?>
+                    <a href="./controllers/Users.php?q=logout"><li>Logout</li></a>
+                <?php endif; ?>
+
             </ul>
 
     </nav>

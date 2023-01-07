5b1ea0a228786e2ce8452673e0baeb43349e475c
deez-blip <gaspard.castagnet92@gmail.com>
2023-01-07 18:03:38 +0100

    :construction: avancement onepage

--- api/onepage.js
+++ api/onepage.js
@@ -1,6 +1,7 @@
 let filmId = document.querySelector('.filmId')
 let currentFilm = "https://api.themoviedb.org/3/movie/" + filmId.value + "?api_key=4d96b3b4809a91b441704c4ff361ba94&language=fr-FR"
 let currentFilmCredit = "https://api.themoviedb.org/3/movie/"+ filmId.value +"/credits?api_key=4d96b3b4809a91b441704c4ff361ba94&language=fr-FR"
+
 let container = document.querySelector(".container")
 let title = document.querySelector(".title")
 let description = document.querySelector(".description")
@@ -14,53 +15,6 @@ let acteurName2 = document.querySelector(".acteur-name-2")
 
 
 // Affiche les films au chargement
-// window.onload = function() {
-
-//     // Affiche tout les films
-//     fetch(currentFilm)
-//     .then((response) => 
-//         response.json())
-
-//     .then(function(data) {
-    
-//         container.innerHTML += `
-//                     <img src="${"https://image.tmdb.org/t/p/original" + data.backdrop_path}" alt="" class="poster">
-//                     <div class="info">
-//                         <div class="title">
-//                             ${data.title}
-//                         </div>
-//                         <div class="description">
-//                             ${data.overview}
-//                         </div>
-//                         <div class="actors">
-//                         `
-//                             fetch(currentFilmCredit)
-//                             .then((response) => 
-//                                 response.json())
-                            
-//                             .then(function(data) {
-//                                 `
-//                                     <div class="actor">
-//                                         <img src="${"https://image.tmdb.org/t/p/original" + data.cast[0].profile_path}" alt="${data.cast[0].name}">
-//                                         <div class="name">
-//                                             ${data.cast[0].name}
-//                                         </div>
-//                                     </div>
-//                                     <div class="actor">
-//                                         <img src="${"https://image.tmdb.org/t/p/original" + data.cast[0].profile_path}" alt="${data.cast[0].name}">
-//                                         <div class="name">
-//                                             ${data.cast[0].name}
-//                                         </div>
-//                                     </div>
-//                                 `
-//                             })
-//                             `
-//                         </div>
-//                     </div>
-//             `
-//         })
-
-// };
 
 window.onload = function() {
 
@@ -70,7 +24,8 @@ window.onload = function() {
 
     .then(function(data) {
 
-        img.src = ` ${"https://image.tmdb.org/t/p/original" + data.backdrop_path} `
+        console.log(data)
+        img.src = ` ${"https://image.tmdb.org/t/p/original" + data.poster_path} `
         title.innerHTML += `${data.title}`
         description.innerHTML += `${data.overview}`
     })
@@ -85,7 +40,6 @@ window.onload = function() {
         acteurName1.innerHTML += `${data.cast[0].name}`
 
         acteur2.src = ` ${"https://image.tmdb.org/t/p/original" + data.cast[1].profile_path} `
-        console.log(data.cast[1])
         acteurName2.innerHTML += `${data.cast[1].name}`
     })
 }

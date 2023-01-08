a8fb05d63382e705ede6df87a420111243049f5d
deez-blip <gaspard.castagnet92@gmail.com>
2023-01-08 15:20:18 +0100

    :art: avancement css page index + css -> tailwind

--- index.php
+++ index.php
@@ -23,44 +23,53 @@
     ?> </h1>
 <br><br><br>
 
-    <input type="text" class="inputSearch border-solid border-4" id="inputSearch">
-    <button class="buttonWriteSearch">search</button>
+    <div class="searchBar flex flex-row align-middle justify-center gap-10">
+        <input type="text" class="inputSearch border-solid border-4 w-2/5" id="inputSearch">
+        <button class="buttonWriteSearch">search</button>
+    </div>
 
     <br>
+    <div class="filter-container flex justify-around text-lg mt-2">
+        <div class="catagorie">
+            <label for="" class="text-white">Choisir une categorie</label>
+            <select id="catego" class="genre">
+                <option value="">Aucun</option>
+                <option value="Action">Action</option>
+                <option value="Aventure">Aventure</option>
+                <option value="Animation">Animation</option>
+                <option value="Crime">Crime</option>
+                <option value="Policier">Policier</option>
+            </select>
+        </div>
 
-    <label for="">Choisir une categorie</label>
-    <select id="catego" class="genre">
-        <option value="">Aucun</option>
-        <option value="Action">Action</option>
-        <option value="Aventure">Aventure</option>
-        <option value="Animation">Animation</option>
-        <option value="Crime">Crime</option>
-        <option value="Policier">Policier</option>
-    </select>
+        <div class="tri">
+            <label for="" class="text-white">Trier par : </label>
+            <select id="tri" class="tri_filter">
+                <option value="">Aucun</option>
+                <option value="nom">Nom</option>
+                <option value="avis">Avis</option>
+                <option value="popularite">Popularité</option>
+                <option value="note">Note</option>
+            </select>
+            <button id="croissantBtn" class="croissant border-solid border-blue-200 border-4">Croissant</button>
+        </div>
 
-    <label for="">Trier par : </label>
-    <select id="tri" class="tri_filter">
-        <option value="">Aucun</option>
-        <option value="nom">Nom</option>
-        <option value="avis">Avis</option>
-        <option value="popularite">Popularité</option>
-        <option value="note">Note</option>
-    </select>
+        <div class="classifiction">
+            <label for="" class="text-white">Limite d'age</label>
+            <select id="" class="limiteAge">
+                <option value="">Aucun</option>
+                <option value="all">Tout public</option>
+                <option value="12">12+</option>
+                <option value="16">16+</option>
+                <option value="18">18+</option>
+            </select>
+        </div>
 
-    <button id="croissantBtn" class="croissant border-solid border-blue-200 border-4">Croissant</button>
+        <button class="searchButton text-white">Chercher</button>
 
-    <label for="">Limite d'age</label>
-    <select id="" class="limiteAge">
-        <option value="">Aucun</option>
-        <option value="all">Tout public</option>
-        <option value="12">12+</option>
-        <option value="16">16+</option>
-        <option value="18">18+</option>
-    </select>
+    </div>
 
-    <button class="searchButton">Chercher</button>
-
-    <div class="grid grid-cols-3 p-40 grid-flow-row gap-4 accueil"></div>
+    <div class="grid grid-cols-3 px-40 py-32 grid-flow-row gap-4 accueil"></div>
 
 
     <script src="api/accueil.js"></script>
